fb52f4dda79eb3f2d13db392f961d724130386f8
wanghailong <1464429222>
2024-04-25 16:14:08 +0800

    feat: 修改打包vue.config.js 配置

--- vue.config.js
+++ vue.config.js
@@ -37,7 +37,7 @@ module.exports = defineConfig({
   productionSourceMap: false, // 如果你不需要生产环境的 source map，可以将其设置为 false 以加速生产环境构建
   outputDir: path.join(__dirname, packageName), // 当运行 vue-cli-service build 时生成的生产环境构建文件的目录。注意目标目录的内容在构建之前会被清除 (构建时传入 --no-clean 可关闭该行为)。 default: dist
   configureWebpack: { // 如果这个值是一个对象，则会通过 webpack-merge 合并到最终的配置中。 如果这个值是一个函数，则会接收被解析的配置作为参数。该函数既可以修改配置并不返回任何东西，也可以返回一个被克隆或合并过的配置版本。
-    watch: true, //启用 Watch 模式。这意味着在初始构建之后，webpack 将继续监听任何已解析文件的更改
+    watch: false, //启用 Watch 模式。这意味着在初始构建之后，webpack 将继续监听任何已解析文件的更改  // 打包进行变更为false 否则进程将一直执行。
     entry: { // 打包入口 需要配置 content background popup
       content:  "/src/content-script/content-script.js",
       background:  "/src/background/background.js",
